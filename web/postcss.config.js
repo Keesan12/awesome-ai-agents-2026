6dc49bbc0fab3761d9d3f9303dd7e04be4974778
ARUNAGIRINATHAN_K <Arun31.march.2k6@gmail.com>
2026-07-13 22:55:17 +0530

    chore: migrate postcss config to ESM to fix Next.js 16 webpack compile crash

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,2 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// Migrated to postcss.config.mjs to support Next.js 16 and Tailwind v4.
+// This file is obsolete and can be safely deleted.
