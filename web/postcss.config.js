8becc43e25647ae9b3d00f6162ce440770b2556f
ARUNAGIRINATHAN_K <Arun31.march.2k6@gmail.com>
2026-07-13 23:36:01 +0530

    roll-back

--- web/postcss.config.js
+++
@@ -1,2 +0,0 @@
-// Migrated to postcss.config.mjs to support Next.js 16 and Tailwind v4.
-// This file is obsolete and can be safely deleted.
--- web/tailwind.config.js
+++
@@ -1,2 +0,0 @@
-// Migrated theme settings to web/src/app/globals.css using Tailwind v4 @theme directive.
-// This configuration file is obsolete and can be safely deleted.
